db5062de9fdb4f2d643633c132cc099ca864bb39
林筱珮 <lin@n-works.link>
2022-04-20 10:02:24 +0900

    記事詳細ページまで取得

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styles from "./layout.module.css"
+import utilStyles from "../styles/utils.module.css"
+
+const name = "peipei"
+export const siteTitle = "Next.js blog"
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/fsvicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img src="/images/profile.png" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '../styles/Home.module.css'
+import utilStyles from '../styles/utils.module.css'
+import { getPostsData } from '../lib/post'
+
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+// SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); //mdデータの場合
+  // const postDta = await fetch('endpoint'); //外部API
+  console.log(allPostsData);
+ 
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// SSRの場合
+// export async function getServerSideProps(context) {
+//   return (
+//     props: {
+//       // コンポーネントに渡すためのprops
+//     },
+//   )
+// }
+
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>ぺいぺいですよ！！！！！</p>
+      </section>
+
+      <section>
+        <h2>peipeiのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id,title,date,thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} className={styles.thumbnailImage} />
+              </Link>
+              <Link href={`posts/${id}`}>
+                <a className={utilStyles.boldText}>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>{date}</small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  )
+}
